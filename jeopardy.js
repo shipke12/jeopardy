2b95f61158e7f0207c304c1884ac4adc6b6d5e16
hipkesl1803 <hipkesl1803@uwec.edu>
2020-04-30 23:22:09 -0500

    trying this

--- jeopardy.js
+++ jeopardy.js
@@ -1,6 +1,11 @@
 const question = document.getElementById('question');
 const answer = document.getElementById('answer');
 const returnButton = document.getElementById('return-button');
+const q1c1 = document.getElementById('q1c1');
+
+q1c1.addEventListener('click', () => {
+    question.innerHTML = "question1";
+})
 
 question.addEventListener('click', () => {
     question.setAttribute('style', 'display: none');
