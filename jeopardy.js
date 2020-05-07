1e5fe3b2daae828f7d472da4520bd98877b736b5
hipkesl1803 <hipkesl1803@uwec.edu>
2020-05-07 16:49:24 -0500

    ok quit playing bruh

--- jeopardy.js
+++ jeopardy.js
@@ -1,9 +1,11 @@
 const question = document.getElementById('question');
 const answer = document.getElementById('answer');
 const returnButton = document.getElementById('return-button');
+const multipleChoice = document.getElementById('multiple-choice');
 
 question.addEventListener('click', () => {
     question.setAttribute('style', 'display: none');
+    multipleChoice.setAttribute('style', 'display: none');
     answer.setAttribute('style', 'display: block');
     returnButton.setAttribute('style', 'display: block');
 })
