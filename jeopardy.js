13204d04d2d73bdb2dbe27b60f3b4564c4c297a0
hipkesl1803 <hipkesl1803@uwec.edu>
2020-05-07 17:07:16 -0500

    fuck you

--- jeopardy.js
+++ jeopardy.js
@@ -1,11 +1,9 @@
 const question = document.getElementById('question');
 const answer = document.getElementById('answer');
 const returnButton = document.getElementById('return-button');
-const multipleChoice = document.getElementById('multiple-choice');
 
 question.addEventListener('click', () => {
     question.setAttribute('style', 'display: none');
     answer.setAttribute('style', 'display: block');
     returnButton.setAttribute('style', 'display: block');
-    multipleChoice.setAttribute('style', 'display: block');
 })
